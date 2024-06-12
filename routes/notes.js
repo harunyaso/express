334731c2d93c1dfa86d25fa264a02a0ed615250c
若杉春姫 <wakasugiharuhi@wakasugiharuhimenoMacBook-Air.local>
2024-06-12 14:14:16 +0900

    Add mongodb access

--- routes/notes.js
+++ routes/notes.js
@@ -3,19 +3,19 @@ var router = express.Router();
 
 //接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri ="mongodb+srv:2201130011aw:Haru!230@cluster0.rh23sub.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://haruhi:L0******@cluster0.rh23sub.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const client = new MongoClient(uri);
 
-router.get('/',async(req,res) => {
-	//データベース、コレクションを指定
-	const detabese = client.db('notes');
+router.get('/', async (req, res) => {
+	// データベース、コレクションを指定
+	const database = client.db('notes');
 	const notes = database.collection('notes');
-
-	//id が１のドキュメントを取得
-	const query = {id:2};
+	
+	// idが１のドキュメントを取得
+	const query = { id: 2 };
 	const note = await notes.findOne(query);
-
-	res.json(note.title);
-})
-
-module.exports = router;
+	
+	res.json(note);
+	})
+	
+	module.exports = router;
